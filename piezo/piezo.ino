a200303c5bd923aee88999f3ef37b8df8283f38f
Lukas Hlobil <lhlobil@gmail.com>
2017-04-02 23:02:57 +0200

    czech folklore song "postovsky panacek"

--- piezo/piezo.ino
+++ piezo/piezo.ino
@@ -1,24 +1,44 @@
-int sensorValue;
-int sensorLow = 1023;
-int sensorHigh = 0;
+#include "pitches.h"
 
-const int ledPin = 13;
+int swSequence[] = {
+    NOTE_G1, NOTE_D1, NOTE_C1, NOTE_B1, NOTE_A1, NOTE_D1, NOTE_C1, NOTE_B1, NOTE_A1, NOTE_D1,
+    NOTE_C1, NOTE_B1, NOTE_C1, NOTE_A1, NOTE_D1, NOTE_D1, NOTE_G1, NOTE_D1,
+    NOTE_C1, NOTE_B1, NOTE_A1, NOTE_D1, NOTE_C1, NOTE_B1, NOTE_A1, NOTE_D1, NOTE_C1, NOTE_B1, NOTE_C1, NOTE_A1, NOTE_D1, NOTE_D1, NOTE_E1,
+    NOTE_E1, NOTE_E1, NOTE_C1, NOTE_B1, NOTE_A1, NOTE_G1, NOTE_G1, NOTE_A1, NOTE_B1, NOTE_A1, NOTE_E1, NOTE_FS1, NOTE_D1, NOTE_D1, NOTE_E1,
+    NOTE_E1, NOTE_C1, NOTE_B1, NOTE_A1, NOTE_G1, NOTE_D1, NOTE_A1, NOTE_D1, NOTE_D1
+    };
+
+int postovsky[] = {
+  NOTE_E4, NOTE_G4, NOTE_C4, NOTE_E4, NOTE_G4, NOTE_G3, NOTE_G3, NOTE_C4, NOTE_E4, NOTE_G4,
+  NOTE_E4, NOTE_G4, NOTE_C4, NOTE_E4, NOTE_G4, NOTE_G3, NOTE_G3, NOTE_C4,
+  NOTE_G4, NOTE_G3, NOTE_G3, NOTE_C4, NOTE_E4, NOTE_G4, NOTE_G4, NOTE_G3, NOTE_G3, NOTE_C4, NOTE_E4, NOTE_G4,
+  NOTE_E4, NOTE_G4, NOTE_C4, NOTE_E4, NOTE_G4, NOTE_G3, NOTE_G3, NOTE_C4
+};
+
+int postovskyDuration[] = {
+  2, 4, 2, 4, 4, 4, 4, 4, 4, 4,
+  2, 4, 2, 4, 4, 4, 4, 2,
+  4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+  2, 4, 2, 4, 4, 4, 4, 2 
+};
+
+int swDuration[] = {
+    4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+    4, 4, 4, 4, 4, 4, 4, 4,
+    4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+    4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+    4, 4, 4, 4, 4, 4, 4, 4, 4    
+};
 
 void setup() {
-    pinMode(ledPin, OUTPUT);
-    digitalWrite(ledPin, HIGH);
-
-    while (millis() < 5000) {
-        sensorValue = analogRead(A0);
-        if (sensorValue > sensorHigh) sensorHigh = sensorValue;
-        if (sensorValue < sensorLow) sensorLow = sensorValue;
-    }
-    digitalWrite(ledPin, LOW);
+  for (int thisNote = 0; thisNote < sizeof(postovsky); thisNote++) {
+    int noteDuration = 1000 / postovskyDuration[thisNote];
+    tone(8, postovsky[thisNote], noteDuration);
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+    noTone(8);
+  }
 }
 
 void loop() {
-    sensorValue = analogRead(A0);
-    int pitch = map(sensorValue, sensorLow, sensorHigh, 50, 4000);
-    tone(8, pitch, 20);
-    delay(10);
 }
